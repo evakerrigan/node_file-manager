3bb6a392306f65aa0f6f72ab7f42ee6c07f23a12
Eva Kerrigan <evakerrigan@gmail.com>
2024-02-05 23:42:45 +0600

    fix: refactor rm

--- src/fs/rm.js
+++ src/fs/rm.js
@@ -5,16 +5,19 @@ import path from "path";
 export const rm = (fileName, currentDir) => {
   const filePath = path.join(currentDir, fileName);
   log.cyan("run remove");
-  try {
-    if (fs.existsSync(filePath)) {
-      fs.unlink(filePath, (err) => {
-        if (err) throw err;
-        log.green(`${fileName} has been successfully deleted from the directory ${currentDir}!`);
-      });
-    } else {
+
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
       log.red(`${fileName} does not exist in the directory!`);
+      return;
     }
-  } catch (err) {
-    log.red(`Error deleting the file: ${err}`);
-  }
+
+    fs.unlink(filePath, (err) => {
+      if (err) {
+        log.red(`Error deleting the file: ${err}`);
+        return;
+      }
+      log.green(`${fileName} has been successfully deleted from the directory ${currentDir}!`);
+    });
+  });
 };
