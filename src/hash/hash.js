29b0e5a9e0f318ee88b3f9ab777bc072ecaae264
Eva Kerrigan <evakerrigan@gmail.com>
2024-02-05 23:50:36 +0600

    fix: refactor hash

--- src/hash/hash.js
+++ src/hash/hash.js
@@ -5,12 +5,12 @@ import { log } from "../utils/colorConsole/colorConsole.js";
 export const calcHash = (filePath) => {
   log.cyan("run hash");
 
-  if (!fs.existsSync(filePath)) {
-    log.red("File does not exist");
-    return;
-  }
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      log.red("File does not exist");
+      return;
+    }
 
-  try {
     const hash = crypto.createHash("sha256");
     const input = fs.createReadStream(filePath);
 
@@ -26,7 +26,5 @@ export const calcHash = (filePath) => {
     input.on("error", (err) => {
       log.red(`Error reading the file: ${err}`);
     });
-  } catch (err) {
-    log.red(`Error creating the file read stream: ${err}`);
-  }
+  });
 };
