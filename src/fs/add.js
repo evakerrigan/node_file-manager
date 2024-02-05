3ef4544547abf4dcf96e2e913b5f7cdb041bdc83
Eva Kerrigan <evakerrigan@gmail.com>
2024-02-05 23:08:10 +0600

    fix: refactor add and cat

--- src/fs/add.js
+++ src/fs/add.js
@@ -6,16 +6,18 @@ export const add = (newFileName, currentDir) => {
   log.cyan("run add");
   const filePath = path.join(currentDir, newFileName);
   try {
-    if (fs.existsSync(filePath)) {
-      log.red(`${newFileName} already exists in the directory!`);
-    } else {
-      fs.writeFile(filePath, "", (err) => {
-        if (err) throw err;
-        console.log(
-          `${newFileName} has been successfully created in the directory ${currentDir}!`
-        );
-      });
-    }
+    fs.access(filePath, (err) => {
+      if (!err) {
+        log.red(`${newFileName} already exists in the directory!`);
+      } else {
+        fs.writeFile(filePath, "", (err) => {
+          if (err) throw err;
+          log.green(
+            `${newFileName} has been successfully created in the directory ${currentDir}!`
+          );
+        });
+      }
+    });
   } catch (err) {
     log.red(`Error creating the file: ${err}`);
   }
--- src/fs/cat.js
+++ src/fs/cat.js
@@ -1,25 +1,22 @@
 import fs from "fs";
-import path from "path"; 
+import path from "path";
 import { log } from "../utils/colorConsole/colorConsole.js";
 
-export const cat = (fileName, currentDir) => {
+export const cat = async (fileName, currentDir) => {
   const fullFilePath = path.join(currentDir, fileName);
   log.cyan("run cat");
   try {
-    if (fs.existsSync(fullFilePath)) {
-      const readStream = fs.createReadStream(fullFilePath, { encoding: "utf8" });
-      let data = "";
-      readStream.on('data', (chunk) => {
-        data += chunk; 
-      });
-      readStream.on('end', () => {
-        log.green(`The content of the file ${fileName}:`);
-        console.log(data); 
-      });
-    } else {
-      log.red(`${fileName} does not exist in the directory!`);
-    }
+    await fs.promises.access(fullFilePath);
+    const readStream = fs.createReadStream(fullFilePath, { encoding: "utf8" });
+    let data = "";
+    readStream.on("data", (chunk) => {
+      data += chunk;
+    });
+    readStream.on("end", () => {
+      log.green(`The content of the file ${fileName}:`);
+      console.log(data);
+    });
   } catch (err) {
-    log.red(`Error reading the file: ${err}`);
+    log.red(`${fileName} does not exist in the directory!`);
   }
 };
