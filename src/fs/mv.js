d182977b979856dc67ebda124237305a0d5a5186
Eva Kerrigan <evakerrigan@gmail.com>
2024-02-05 23:37:07 +0600

    fix: refactor mv

--- src/fs/mv.js
+++ src/fs/mv.js
@@ -7,19 +7,28 @@ export const mv = (filePath, newDir) => {
   const fileName = path.basename(filePath);
   const newFilePath = path.join(newDir, fileName);
 
-  try {
-    if (fs.existsSync(filePath)) {
-      const readStream = fs.createReadStream(filePath);
-      const writeStream = fs.createWriteStream(newFilePath);
-      readStream.pipe(writeStream);
-      readStream.on("end", () => {
-        log.green(`${fileName} has been successfully moved to ${newDir}`);
-        fs.unlinkSync(filePath);
-      });
-    } else {
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
       log.red(`${fileName} does not exist in the directory!`);
+      return;
     }
-  } catch (err) {
-    log.red(`Error moving the file: ${err}`);
-  }
+
+    const readStream = fs.createReadStream(filePath);
+    const writeStream = fs.createWriteStream(newFilePath);
+    readStream.pipe(writeStream);
+    readStream.on("end", () => {
+      log.green(`${fileName} has been successfully moved to ${newDir}`);
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          log.red(`Error deleting the file: ${err}`);
+        }
+      });
+    });
+    readStream.on("error", (err) => {
+      log.red(`Error reading the file: ${err}`);
+    });
+    writeStream.on("error", (err) => {
+      log.red(`Error writing the file: ${err}`);
+    });
+  });
 };
